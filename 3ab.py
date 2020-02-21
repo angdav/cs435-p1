a4ffd7dfafc9162c835f67e3dd5cfc580c97498d
David Ang <cdang1024@gmail.com>
2020-02-20 20:07:53 -0500

    added completed 3ab.py

---
+++ 3ab.py
@@ -0,0 +1,21 @@
+# Problem 3 Parts A and B (Arrays of Integers)
+
+import random
+
+def getRandomArray(n):
+    '''returns array of size n containing random unique values'''
+    tempset = set()
+    retarr = []
+    while len(retarr) < n:
+        r = random.randint(0,n*10)
+        if n not in tempset:
+            tempset.add(r)
+            retarr.append(r)
+    return retarr
+
+def getSortedArray(n):
+    '''return array of size n with integers from n to 1'''
+    return [n-i for i in range(n)]
+
+print(getSortedArray(50))
+print(getRandomArray(50))
